af09daf88e5e93d15816c84a27e92768866440ea
Caitlyn Carter <cecarter1@aggies.ncat.edu>
2025-10-06 13:05:17 -0400

    Powerup Setup

---
+++ Assets/Scripts/Sciptables/PowerUps/PowerUp.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PowerUp : Ability
+{
+    public float powerupSec;
+    public abstract void Deactivate(GameObject target);
+
+    public IEnumerator StartPowerupCountdown(GameObject target)
+    {
+        yield return new WaitForSeconds(powerupSec);
+        Deactivate(target);
+    }
+}
