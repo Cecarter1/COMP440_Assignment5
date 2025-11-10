1e881fb181b6602458f87dae1994ae5067005326
Caitlyn Carter <113126773+Cecarter1@users.noreply.github.com>
2025-11-10 13:44:20 -0500

    Add files via upload

---
+++ Assets/Scripts/Objects/Checkpoint.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.tag == "Player")
+        {
+            animator.SetTrigger("Disabled");
+        }
+    }
+}
